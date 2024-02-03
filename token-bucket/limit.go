804e05689cc3f71c66e1cd9b93fd72748496421d
shivam <shivam.kumar@matchmyflight.com>
2024-02-04 03:19:25 +0530

    token-bucket approach

---
+++ token-bucket/limit.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"golang.org/x/time/rate"
+	"net/http"
+)
+
+func rateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			message := Message{
+				Status: "Request Failed",
+				Body:   "The API is full at capacity, try again later",
+			}
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(&message)
+			return
+		} else {
+			next(w, r)
+		}
+	})
+}
